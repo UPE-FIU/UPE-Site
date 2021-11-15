19a4d0da0c6e85ea8af2d69ee3886246e2d606c8
Ziad Malik <zmali002@fiu.edu>
2021-11-14 23:16:17 -0500

    EBoard Calls Added

--- src/apiCalls/eBoardCall.ts
+++ src/apiCalls/eBoardCall.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function getData(): Promise<any> {
   const url =
-    "https://vh4r0zrl.api.sanity.io/v1/data/query/production?query=*%5B_type%20%3D%3D%20%22links%22%5D";
+    "https://vh4r0zrl.api.sanity.io/v1/data/query/production?query=*%5B_type%20%3D%3D%20%22eboard%22%5D";
   try {
     const response = await axios.get(url);
     if (response.status == 200) {
--- src/apiCalls/eventCall.ts
+++ src/apiCalls/eventCall.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function getData(): Promise<any> {
   const url =
-    "https://vh4r0zrl.api.sanity.io/v1/data/query/production?query=*%5B_type%3D%3D'eboard'%5D%7Beboard_github%2C%20eboard_name%2C%20eboard_image%7D";
+    "https://vh4r0zrl.api.sanity.io/v1/data/query/production?query=*%5B_type%20%3D%3D%20%22event%22%5D";
   try {
     const response = await axios.get(url);
     if (response.status == 200) {
